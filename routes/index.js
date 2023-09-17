25372dab3a17e4bf9409523a8eb4c743c5107572
vickymalindo <vicky.malindo@gmail.com>
2023-09-17 22:09:40 +0700

    feat: add route for update book

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createBook, getBook, getBooks } from '../controllers/Books.js';
+import {
+  createBook,
+  getBook,
+  getBooks,
+  updateBook,
+} from '../controllers/Books.js';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
 router.get('/book/:id', getBook);
 router.post('/book', createBook);
+router.put('/book/:id', updateBook);
 
 export default router;
