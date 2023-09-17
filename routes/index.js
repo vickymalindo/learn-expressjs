f4e0f17d3817ca4d9ddc22a900848788cd79b045
vickymalindo <vicky.malindo@gmail.com>
2023-09-17 21:50:24 +0700

    feat: add route get detail book

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBook, getBooks } from '../controllers/Books.js';
+import { createBook, getBook, getBooks } from '../controllers/Books.js';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
+router.get('/book/:id', getBook);
 router.post('/book', createBook);
 
 export default router;
