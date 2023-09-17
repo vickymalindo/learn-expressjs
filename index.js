48f99693e86d8a5439f06712bdd548f92d1d93f8
vickymalindo <vicky.malindo@gmail.com>
2023-09-17 20:12:21 +0700

    feat: connect to db

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import db from './config/Database.js';
 const app = express();
+
+try {
+  await db.authenticate();
+  console.log('Database Connected');
+} catch (error) {
+  console.error(error);
+}
 
 app.get('/', function (req, res) {
   res.send('Hello Vicky, you are the great person');
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('server run on port 3000'));
