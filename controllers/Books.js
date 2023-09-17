a767c87aac90eacd7cb9e7022f0f97a8362a23db
vickymalindo <vicky.malindo@gmail.com>
2023-09-17 22:09:00 +0700

    feat: add update book function

--- controllers/Books.js
+++ controllers/Books.js
@@ -33,3 +33,19 @@ export const getBook = async (req, res) => {
   if (!book) return res.status(404).json({ message: 'Book not found' });
   return res.status(200).json({ message: 'Get book successfully', data: book });
 };
+
+export const updateBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const bookUpdated = await Books.update(req.body, {
+      where: {
+        id: id,
+      },
+    });
+    if (bookUpdated[0] === 0)
+      return res.status(400).json({ message: 'Update book failed' });
+    return res.status(202).json({ message: 'Update book successfully' });
+  } catch (error) {
+    throw new Error(`Update book failed: ${error.message}`);
+  }
+};
