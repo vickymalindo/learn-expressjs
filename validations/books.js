dc893861f2c9faec275857dbbcbdf4490bde28ce
vickymalindo <vicky.malindo@gmail.com>
2023-09-18 23:25:15 +0700

    refactor: change Titile to Title

--- validations/books.js
+++ validations/books.js
@@ -2,7 +2,7 @@ import { check } from 'express-validator';
 
 export const insertBookValidator = () => {
   return [
-    check('title').not().isEmpty().withMessage('Titile is required'),
+    check('title').not().isEmpty().withMessage('Title is required'),
     check('author').not().isEmpty().withMessage('Author is required'),
     check('category').not().isEmpty().withMessage('Category is required'),
   ];
