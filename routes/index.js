1f0917389e7519d833bf37c477c3a42a2997636e
vickymalindo <vicky.malindo@gmail.com>
2023-09-18 23:22:05 +0700

    feat: implement rules validation to route create book

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ import {
   getBooks,
   updateBook,
 } from '../controllers/Books.js';
+import { insertBookValidator } from '../validations/books.js';
 
 const router = express.Router();
 
 router.get('/books', getBooks);
 router.get('/book/:id', getBook);
-router.post('/book', createBook);
+router.post('/book', insertBookValidator(), createBook);
 router.put('/book/:id', updateBook);
 router.delete('/book/:id', deleteBook);
 
