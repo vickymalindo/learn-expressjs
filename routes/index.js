a7957a64ac56b960a4400798cf030d037034634c
vickymalindo <vicky.malindo@gmail.com>
2023-09-20 21:31:48 +0700

    feat: implement register validation in routes

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,20 @@ import {
   getBooks,
   updateBook,
 } from '../controllers/Books.js';
+import { register } from '../controllers/Users.js';
 import { insertBookValidator } from '../validations/books.js';
+import { registerValidator } from '../validations/users.js';
 
 const router = express.Router();
 
+// Books Route
 router.get('/books', getBooks);
 router.get('/book/:id', getBook);
 router.post('/book', insertBookValidator(), createBook);
 router.put('/book/:id', insertBookValidator(), updateBook);
 router.delete('/book/:id', deleteBook);
+
+// Users Route
+router.post('/register', ...registerValidator(), register);
 
 export default router;
