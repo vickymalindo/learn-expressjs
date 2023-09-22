76ac8198e48915351a5bd08f59a993eb5920049e
vickymalindo <vicky.malindo@gmail.com>
2023-09-22 21:33:36 +0700

    feat: implement route refresh token

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,23 @@ import {
   getBooks,
   updateBook,
 } from '../controllers/Books.js';
-import { login, register } from '../controllers/Users.js';
+import { login, refreshToken, register } from '../controllers/Users.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 import { insertBookValidator } from '../validations/books.js';
 import { loginValidator, registerValidator } from '../validations/users.js';
 
 const router = express.Router();
 
-// Books Route
+// Books Routes
 router.get('/books', verifyToken, getBooks);
 router.get('/book/:id', getBook);
 router.post('/book', insertBookValidator(), verifyToken, createBook);
 router.put('/book/:id', insertBookValidator(), updateBook);
 router.delete('/book/:id', deleteBook);
 
-// Users Route
+// Users Routes
 router.post('/register', ...registerValidator(), register);
 router.post('/login', ...loginValidator(), login);
+router.get('/token', refreshToken);
 
 export default router;
