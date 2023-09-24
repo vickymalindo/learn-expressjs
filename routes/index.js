d7e69966124b7ea3505527ae66a240cd998797ec
vickymalindo <vicky.malindo@gmail.com>
2023-09-24 20:51:57 +0700

    feat: implement route get all user and change verifyToken to auth

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,14 @@ import {
   getBooks,
   updateBook,
 } from '../controllers/Books.js';
-import { login, logout, refreshToken, register } from '../controllers/Users.js';
-import { verifyToken } from '../middleware/verifyToken.js';
+import {
+  getUsers,
+  login,
+  logout,
+  refreshToken,
+  register,
+} from '../controllers/Users.js';
+import { verifyToken as auth } from '../middleware/verifyToken.js';
 import { insertBookValidator } from '../validations/books.js';
 import { loginValidator, registerValidator } from '../validations/users.js';
 
@@ -16,14 +22,15 @@ const router = express.Router();
 // Books Routes
 router.get('/books', getBooks);
 router.get('/book/:id', getBook);
-router.post('/book', insertBookValidator(), verifyToken, createBook);
-router.put('/book/:id', verifyToken, insertBookValidator(), updateBook);
-router.delete('/book/:id', verifyToken, deleteBook);
+router.post('/book', insertBookValidator(), auth, createBook);
+router.put('/book/:id', auth, insertBookValidator(), updateBook);
+router.delete('/book/:id', auth, deleteBook);
 
 // Users Routes
 router.post('/register', ...registerValidator(), register);
 router.post('/login', ...loginValidator(), login);
 router.get('/token', refreshToken);
 router.delete('/logout', logout);
+router.get('/users', auth, getUsers);
 
 export default router;
