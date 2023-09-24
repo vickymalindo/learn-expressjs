3b73866936e007d206fdc781cce0f5917a88ad98
vickymalindo <vicky.malindo@gmail.com>
2023-09-24 22:27:07 +0700

    feat: add freeze table

--- models/CartModel.js
+++ models/CartModel.js
@@ -3,13 +3,19 @@ import db from '../config/Database.js';
 
 const { DataTypes } = Sequelize;
 
-const Carts = db.define('carts', {
-  user_id: {
-    type: DataTypes.INTEGER,
+const Carts = db.define(
+  'carts',
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+    },
   },
-  book_id: {
-    type: DataTypes.INTEGER,
-  },
-});
+  {
+    freezeTableName: true,
+  }
+);
 
 export default Carts;
