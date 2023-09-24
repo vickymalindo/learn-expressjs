debc7fb8de1a4121b3ff4eeeed6d6264f82f0079
vickymalindo <vicky.malindo@gmail.com>
2023-09-24 19:36:00 +0700

    feat: add cart model

---
+++ models/CartModel.js
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+import db from '../config/Database.js';
+
+const { DataTypes } = Sequelize;
+
+const Carts = db.define('carts', {
+  user_id: {
+    type: DataTypes.INTEGER,
+  },
+  book_id: {
+    type: DataTypes.INTEGER,
+  },
+});
+
+export default Carts;
