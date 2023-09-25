457c44ef97aac7ba3ee5e44a2eed6bc8f01d8bf5
vickymalindo <vicky.malindo@gmail.com>
2023-09-25 21:38:33 +0700

    refactor: delete import

--- models/CartModel.js
+++ models/CartModel.js
@@ -1,7 +1,5 @@
 import { Sequelize } from 'sequelize';
 import db from '../config/Database.js';
-import Users from './UserModel.js';
-import Books from './BookModel.js';
 
 const { DataTypes } = Sequelize;
 
