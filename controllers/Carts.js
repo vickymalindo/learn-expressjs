ae5e012041743c5329ad994255eafdd59e3edb8f
vickymalindo <vicky.malindo@gmail.com>
2023-09-25 21:40:26 +0700

    feat: add get user carts function

--- controllers/Carts.js
+++ controllers/Carts.js
@@ -1,4 +1,6 @@
+import Books from '../models/BookModel.js';
 import Carts from '../models/CartModel.js';
+import Users from '../models/UserModel.js';
 import response from '../utils/response.util.js';
 import { validationResult } from 'express-validator';
 
@@ -6,15 +8,41 @@ export const createCart = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.json(errors);
   try {
-    const { user_id, book_id } = req.body;
+    const { userId, bookId } = req.body;
     const cart = await Carts.create({
-      user_id: user_id,
-      book_id: book_id,
+      userId: userId,
+      bookId: bookId,
     });
     response({
       statusCode: 201,
       message: 'Insert cart success',
       datas: cart,
+      res,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getUserCarts = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await Users.findAll({
+      where: {
+        id: id,
+      },
+      include: [{ model: Books }],
+      attributes: { exclude: ['password', 'refresh_token'] },
+    });
+    const { books } = result;
+    console.log(books?.length);
+    response({
+      statusCode: 200,
+      message:
+        result[0].books?.length === 0
+          ? 'Empty user carts'
+          : 'Get user carts success',
+      datas: result,
       res,
     });
   } catch (error) {
