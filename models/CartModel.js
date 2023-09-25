05863a219edc2f6d99bbe525390ed5b475eddd51
vickymalindo <vicky.malindo@gmail.com>
2023-09-25 21:37:43 +0700

    feat: change field to empty object

--- models/CartModel.js
+++ models/CartModel.js
@@ -1,18 +1,13 @@
 import { Sequelize } from 'sequelize';
 import db from '../config/Database.js';
+import Users from './UserModel.js';
+import Books from './BookModel.js';
 
 const { DataTypes } = Sequelize;
 
 const Carts = db.define(
   'carts',
-  {
-    user_id: {
-      type: DataTypes.INTEGER,
-    },
-    book_id: {
-      type: DataTypes.INTEGER,
-    },
-  },
+  {},
   {
     freezeTableName: true,
   }
