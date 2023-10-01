41b82ef7d1d2b698dab2e081c70f76cfc3749178
vickymalindo <vicky.malindo@gmail.com>
2023-10-01 23:59:00 +0700

    feat: add validation for image

--- validations/books.js
+++ validations/books.js
@@ -5,5 +5,11 @@ export const insertBookValidator = () => {
     check('title').not().isEmpty().withMessage('Title is required'),
     check('author').not().isEmpty().withMessage('Author is required'),
     check('category').not().isEmpty().withMessage('Category is required'),
+    check('image').custom((value, { req }) => {
+      if (req.file.mimetype.includes('image')) {
+        return true;
+      }
+      throw new Error('Only image');
+    }),
   ];
 };
