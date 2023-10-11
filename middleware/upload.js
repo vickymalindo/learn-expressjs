d6671548cffe986982e92cda989919937a74849d
vickymalindo <vicky.malindo@gmail.com>
2023-10-11 15:30:24 +0700

    feat: change extension to webp

--- middleware/upload.js
+++ middleware/upload.js
@@ -14,7 +14,8 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + '-' + file.originalname);
+    const imageWithoutExt = file.originalname.split('.')[0];
+    cb(null, `${uniqueSuffix}-${imageWithoutExt}.webp`);
   },
 });
 
