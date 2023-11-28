efc0dd93083200994f9d9756b57d415b560d9d6d
vickymalindo <vicky.malindo@gmail.com>
2023-11-28 19:31:47 +0700

    feat: add random token

---
+++ utils/randomToken.js
@@ -0,0 +1,12 @@
+export const generateRandomToken = (length) => {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+  let token = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters.charAt(randomIndex);
+  }
+
+  return token;
+};
